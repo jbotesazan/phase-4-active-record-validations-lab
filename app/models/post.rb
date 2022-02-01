c9d0b26a793f7b56c18e5a0f8bb4052ba9236860
Jason Botesazan <jbotesazan@gmail.com>
2022-02-01 12:19:05 -0500

    commit tree 74131a1721f042db6a3ab9af96079bc97067ce8e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+    validate :clickbaity
+
+    # def clickbaity
+    #     matching = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+    #     post = self.title
+    #     matching.each do |s|
+    #         byebug
+    #         unless post.match(s)
+    #             errors.add(:title, 'Sorry, we are afraid your title is not clickbaity enough.')
+    #         end
+    #     end
+    # end
+
+    def clickbaity
+        matching = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+            unless matching.any? {|s| s.match(title)}
+            errors.add(:title, 'Sorry, we are afraid your title is not clickbaity enough.')
+        end
+    end
+
 end
